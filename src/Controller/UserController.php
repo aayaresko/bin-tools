6105f12bee0540ce89d7494ccb84782b4c66b182
aayaresko <aayaresko@gmail.com>
2019-10-26 14:04:04 +0300

    added pagination to users list.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,22 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends AbstractController
 {
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
         /** @var UserRepository $repository */
         $repository = $this->getDoctrine()->getManager()->getRepository(User::class);
 
-        return $this->render('user/index.html.twig', ['data' => $repository->findBy(['enabled' => true])]);
+        $page = $request->query->getInt('page', 1);
+        $queryBuilder = $repository->getEnabledQueryBuilder();
+        $pagination = $paginator->paginate($queryBuilder, $page, 10);
+
+        return $this->render('user/index.html.twig', compact('pagination'));
     }
 }
